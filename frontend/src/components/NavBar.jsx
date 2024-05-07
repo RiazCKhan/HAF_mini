605c492c6d5eeefba5c4e8cf3a8795d734c01759
RiazK <RiazCKhan@gmail.com>
2024-05-07 16:06:26 -0400

    add nav icons

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,5 +1,24 @@
-import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import ChairIcon from "@mui/icons-material/Chair";
+import MailIcon from "@mui/icons-material/Mail";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import SettingsIcon from "@mui/icons-material/Settings";
+import avatar from "../static/avatarDog.jpeg";
+
+const IconBar = styled(Box)(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  gap: "1.25rem",
+  padding: "0 0.625rem",
+}));
 
 const NavBar = () => {
   return (
@@ -15,6 +34,15 @@ const NavBar = () => {
           </Typography>
         </Box>
         <ChairIcon sx={{ display: { xs: "block", sm: "none" } }} />
+        <IconBar>
+          <Badge color="secondary" badgeContent={0} showZero>
+            <MailIcon />
+          </Badge>
+          <Badge color="secondary" badgeContent={0} showZero>
+            <NotificationsIcon />
+          </Badge>
+          <Avatar src={avatar} />
+        </IconBar>
       </Toolbar>
     </AppBar>
   );
