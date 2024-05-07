4b2e340c32da3a7e1354e91621ccea91fd0d5aaa
RiazK <RiazCKhan@gmail.com>
2024-05-07 18:38:10 -0400

    add side nav text with icons

--- frontend/src/components/Leftbar.jsx
+++ frontend/src/components/Leftbar.jsx
@@ -1,9 +1,62 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import SupportAgentIcon from "@mui/icons-material/SupportAgent";
+import Diversity3Icon from "@mui/icons-material/Diversity3";
+import HandshakeIcon from "@mui/icons-material/Handshake";
+import HealthAndSafetyIcon from "@mui/icons-material/HealthAndSafety";
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
 
 const Leftbar = () => {
   return (
     <Box sx={{ flex: 1, p: 2, display: { xs: "none", sm: "block" } }}>
-      Leftbar
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <SupportAgentIcon />
+            </ListItemIcon>
+            <ListItemText primary="Agent" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <Diversity3Icon />
+            </ListItemIcon>
+            <ListItemText primary="Client" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <HandshakeIcon />
+            </ListItemIcon>
+            <ListItemText primary="Donor" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <HealthAndSafetyIcon />
+            </ListItemIcon>
+            <ListItemText primary="Donation" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <LocalShippingIcon />
+            </ListItemIcon>
+            <ListItemText primary="Delivery" />
+          </ListItemButton>
+        </ListItem>
+      </List>
     </Box>
   );
 };
