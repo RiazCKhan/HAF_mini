137a329d0625ab07bf9e0f563c780a0d055e2748
RiazK <RiazCKhan@gmail.com>
2024-05-07 15:24:08 -0400

    add logo and breakpoint

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,7 +1,23 @@
-import { Box } from "@mui/material";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+import ChairIcon from "@mui/icons-material/Chair";
 
 const NavBar = () => {
-  return <Box>NavBar</Box>;
+  return (
+    <AppBar sx={{ position: "sticky" }}>
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Box sx={{ display: { xs: "none", sm: "block" } }}>
+          <Typography
+            variant="h6"
+            sx={{ display: "flex", alignItems: "center" }}
+          >
+            <ChairIcon sx={{ display: "flex", p: 1 }} />
+            HAF MINI
+          </Typography>
+        </Box>
+        <ChairIcon sx={{ display: { xs: "block", sm: "none" } }} />
+      </Toolbar>
+    </AppBar>
+  );
 };
 
 export default NavBar;
