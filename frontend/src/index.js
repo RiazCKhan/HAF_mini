940bddd2e25e1b6560ed97f0f5837ce172c8eaa8
RiazK <RiazCKhan@gmail.com>
2024-05-08 11:47:55 -0400

    add create broswer router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Homepage from './pages/Homepage';
+import Error from './pages/Error'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Homepage />,
+    errorElement: <Error />
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
