dff888f3a60cbbdf4ff7bdf403b9dea6d08b4ee1
RiazK <RiazCKhan@gmail.com>
2024-05-09 16:42:15 -0400

    add obj str representation

--- backend/agent/models.py
+++ backend/agent/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 class Agent(models.Model):
-  name = models.CharField()
+  full_name = models.CharField()
+  agency = models.CharField()
   email = models.CharField()
   phone_number = models.CharField
   status = models.BooleanField
+
+  def __str__(self):
+    return self.full_name
