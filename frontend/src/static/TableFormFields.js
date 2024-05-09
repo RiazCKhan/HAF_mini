785fc175e157c53c6fb3ec5868ad1a2b3fe4f394
RiazK <RiazCKhan@gmail.com>
2024-05-09 09:43:35 -0400

    add renamed file

---
+++ frontend/src/static/TableFormFields.js
@@ -0,0 +1,109 @@
+export const AgencyFields = [
+  {
+    id: "name",
+    field: "Name",
+    type: "text"
+  },
+  {
+    id: "email",
+    field: "Email",
+    type: "text"
+  },
+  {
+    id: "phoneNumber",
+    field: "Phone Number",
+    type: "number"
+  },
+  {
+    id: "status",
+    field: "Status",
+    type: "text"
+  },
+];
+
+export const ClientFields = [
+  {
+    id: "name",
+    field: "Name",
+    type: "text"
+  },
+  {
+    id: "agent",
+    field: "Agent",
+    type: "text"
+  },
+  {
+    id: "address",
+    field: "Address",
+    type: "text"
+  },
+  {
+    id: "phoneNumber",
+    field: "Phone Number",
+    type: "number"
+  },
+];
+
+export const DeliveryFields = [
+  {
+    id: "deliveryId",
+    field: "Delivery ID",
+    type: "text"
+  },
+  {
+    id: "clientId",
+    field: "Client ID",
+    type: "text"
+  },
+  {
+    id: "referralId",
+    field: "Referral ID",
+    type: "text"
+  },
+  {
+    id: "status",
+    field: "Status",
+    type: "text"
+  },
+];
+
+export const DonationFields = [
+  {
+    id: "donorId",
+    field: "Donor",
+    type: "text"
+  },
+  {
+    id: "itemName",
+    field: "Item Name",
+    type: "text"
+  },
+  {
+    id: "itemQuantity",
+    field: "Item Quantity",
+    type: "text"
+  },
+];
+
+export const DonorFields = [
+  {
+    id: "donorId",
+    field: "Donor",
+    type: "text"
+  },
+  {
+    id: "foundation",
+    field: "Foundation",
+    type: "text"
+  },
+  {
+    id: "email",
+    field: "Email",
+    type: "text"
+  },
+  {
+    id: "phoneNumber",
+    field: "Phone Number",
+    type: "number"
+  },
+];
