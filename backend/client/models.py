9ef4ca33a504c91aed7b6302d28b1afcd89df80c
RiazK <RiazCKhan@gmail.com>
2024-05-09 16:47:06 -0400

    add FK relationship and str relationship

--- backend/client/models.py
+++ backend/client/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
 class Client(models.Model):
-  name = models.CharField()
-  agent = models.CharField()
+  full_name = models.CharField()
+  agent = models.ForeignKey(models.Agent, on_delete=models.CASCADE)
   phone_number = models.CharField()
   address = models.CharField()
+
+  def __str__(self):
+    return self.full_name
