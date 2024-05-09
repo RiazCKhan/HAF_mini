5432b0a1c5884cc431908fd4c437338d548144b9
RiazK <RiazCKhan@gmail.com>
2024-05-09 09:21:31 -0400

    add modal and dynamic render of static form fields

---
+++ frontend/src/components/CreateModal.jsx
@@ -0,0 +1,67 @@
+import { Box, Button, Modal, TextField, styled } from "@mui/material";
+import { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+
+const CreateModal = (props) => {
+  const [openCreateModal, setOpenCreateModal] = useState(false);
+
+  const { tablename, fields } = props;
+
+  const modalFormFields = fields.map((field) => {
+    return (
+      <TextField
+        id="outlined-password-input"
+        label={field.field}
+        type={field.type}
+      />
+    );
+  });
+
+  const MyStyleModal = styled(Modal)({
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  });
+
+  return (
+    <Box>
+      <Button onClick={(event) => setOpenCreateModal(true)}>
+        <AddIcon />
+        New {tablename}
+      </Button>
+      <MyStyleModal
+        open={openCreateModal}
+        onClose={(event) => setOpenCreateModal(false)}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            width: "auto",
+            backgroundColor: "white",
+            p: 3,
+            borderRadius: 2,
+          }}
+        >
+          <Box
+            component="form"
+            sx={{
+              "& .MuiTextField-root": { m: 1, width: "25ch" },
+            }}
+            noValidate
+            autoComplete="off"
+          >
+            <div>{modalFormFields}</div>
+          </Box>
+          <Box sx={{ display: "flex", justifyContent: "end", p: 1 }}>
+            <Button variant="contained">Submit</Button>
+          </Box>
+        </Box>
+      </MyStyleModal>
+    </Box>
+  );
+};
+
+export default CreateModal;
---
+++ frontend/src/static/AgencyFormFields.js
@@ -0,0 +1,22 @@
+export const AgencyFields = [
+  {
+    id: "agency",
+    field: "Agency",
+    type: "text"
+  },
+  {
+    id: "email",
+    field: "Email",
+    type: "text"
+  },
+  {
+    id: "phoneNumber",
+    field: "Phone Number",
+    type: "text"
+  },
+  {
+    id: "status",
+    field: "Status",
+    type: "text"
+  },
+];
