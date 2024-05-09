19327bfadcfd9ef4de815e23b5d2891046aac5af
RiazK <RiazCKhan@gmail.com>
2024-05-09 17:27:52 -0400

    add model and choices

--- backend/referral/models.py
+++ backend/referral/models.py
@@ -1,4 +1,17 @@
 from django.db import models
+from agent.models import Agent
+from client.models import Client
+
+STATUS_CHOICES = (
+    ("DRAFT", "draft"),
+    ("PENDING", "pending"),
+    ("APPROVED", "approved"),
+    ("REQUESTED", "requested"),
+    ("COMPLETE", "complete"),
+)
 
 class Referral(models.Model):
-  pass
+  agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
+  client = models.ForeignKey(Client, on_delete=models.CASCADE)
+  status = models.CharField(choices=STATUS_CHOICES, default="DRAFT")
+  # item stuff
