c4d50c736d1dfe713bd577bba17fc30ad55df036
RiazK <RiazCKhan@gmail.com>
2024-05-10 10:52:09 -0400

    register custom admin

--- backend/client/admin.py
+++ backend/client/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Client
 
+class ClientAdmin(admin.ModelAdmin):
+  list_display = ("full_name", "agent", "phone_number", "address",)
+
 admin.site.register(Client)
