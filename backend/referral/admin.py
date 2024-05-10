bab9d9841c3e14749c19a882d9fa3ed5901469a4
RiazK <RiazCKhan@gmail.com>
2024-05-10 10:24:13 -0400

    add referral models

--- backend/referral/admin.py
+++ backend/referral/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
-from .models import Referral
+from .models import ReferralItem, ReferralOrder, Referral
 
-admin.site.register(Referral)
 
+class ReferralItemAdmin(admin.ModelAdmin):
+    list_display = (
+        "item",
+        "quantity",
+    )
+
+admin.site.register(ReferralItem, ReferralItemAdmin)
+
+
+class ReferralOrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "client",)
+
+admin.site.register(ReferralOrder, ReferralOrderAdmin)
+
+
+class ReferralAdmin(admin.ModelAdmin):
+    list_display = (
+        "client",
+        "agent",
+        "status",
+    )
+
+admin.site.register(Referral, ReferralAdmin)
