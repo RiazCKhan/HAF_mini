2e32d5b61fa4699b33c66655ab34418d099508b1
RiazK <RiazCKhan@gmail.com>
2024-05-10 10:14:52 -0400

    add migration

--- backend/agent/migrations/0001_initial.py
+++ backend/agent/migrations/0001_initial.py
@@ -7,18 +7,25 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Agent',
+            name="Agent",
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('full_name', models.CharField(max_length=100)),
-                ('agency', models.CharField(max_length=100)),
-                ('email', models.CharField(max_length=100)),
-                ('phone_number', models.CharField(max_length=100)),
+                (
+                    "id",
+                    models.BigAutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("full_name", models.CharField(max_length=100)),
+                ("agency", models.CharField(max_length=100)),
+                ("email", models.CharField(max_length=100)),
+                ("phone_number", models.CharField(max_length=100)),
             ],
         ),
     ]
