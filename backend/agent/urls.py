571a7e824f3211a377dab3a84674a5cb72c037f3
RiazK <RiazCKhan@gmail.com>
2024-05-10 12:27:08 -0400

    add api enpoints for list, create, and update

--- backend/agent/urls.py
+++ backend/agent/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import AgentList
+from .views import AgentList, AgentCreate, AgentUpdate
 
 urlpatterns = [
     path("list/", AgentList.as_view()),
+    path("create/", AgentCreate.as_view()),
+    path("update/<int:pk>", AgentUpdate.as_view()),
 ]
--- backend/agent/views.py
+++ backend/agent/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework import generics
-from rest_framework import status
+from rest_framework import generics, status
 
 from .models import Agent
 from .serializers import AgentSerializer
@@ -10,3 +9,22 @@ from .serializers import AgentSerializer
 class AgentList(generics.ListAPIView):
     queryset = Agent.objects.all()
     serializer_class = AgentSerializer
+
+
+class AgentCreate(generics.CreateAPIView):
+    queryset = Agent.objects.all()
+    serializer_class = AgentSerializer
+
+
+class AgentUpdate(generics.RetrieveUpdateAPIView):
+    serializer_class = AgentSerializer
+    queryset = Agent.objects.all()
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response(serializer.data)
