f2d6182fa1f83d7addcdecced341068fe0db1de8
RiazK <RiazCKhan@gmail.com>
2024-05-10 18:37:16 -0400

    add inventory update view and custom exception

--- backend/item/urls.py
+++ backend/item/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import ItemCreate, ItemList, InventoryCreate, InventoryList
+from .views import ItemCreate, ItemList, InventoryCreate, InventoryList, InventoryUpdate
 
 urlpatterns = [
     # Item
     path("listitem/", ItemList.as_view()),
     path("createitem/", ItemCreate.as_view()),
     # Inventory
-    path("addinventory/", InventoryCreate.as_view()),
-    path("inventory/", InventoryList.as_view()),
+    path("inv/", InventoryList.as_view()),
+    path("addinv/", InventoryCreate.as_view()),
+    path("updateinv/<int:pk>", InventoryUpdate.as_view()),
 ]
--- backend/item/views.py
+++ backend/item/views.py
@@ -3,7 +3,15 @@ from rest_framework.response import Response
 from rest_framework import generics, status
 
 from .models import Item, Inventory
-from .serializers import ItemSerializer, ItemInfoSerializer, InventorySerializer, InventoryInfoSerializer
+from .exception import InventoryException
+from .serializers import (
+    ItemSerializer,
+    ItemInfoSerializer,
+    InventorySerializer,
+    InventoryInfoSerializer,
+)
+
+""" ITEM END POINTS """
 
 
 class ItemList(generics.ListAPIView):
@@ -13,7 +21,7 @@ class ItemList(generics.ListAPIView):
         queryset = self.filter_queryset(self.get_queryset())
         serializer = ItemInfoSerializer(queryset, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-    
+
 
 class ItemCreate(generics.CreateAPIView):
     queryset = Item.objects.all()
@@ -24,10 +32,13 @@ class ItemCreate(generics.CreateAPIView):
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-    
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
 
-""" INVENTORY END POINTS """ 
+
+""" INVENTORY END POINTS """
+
 
 class InventoryList(generics.ListAPIView):
     queryset = Inventory.objects.all()
@@ -45,7 +56,29 @@ class InventoryCreate(generics.CreateAPIView):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+
+        new_inventory_item = serializer.validated_data["item"].title
+
+        if Inventory.objects.filter(item__title=new_inventory_item).exists():
+            raise InventoryException
+
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
 
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
+
+
+class InventoryUpdate(generics.RetrieveUpdateAPIView):
+    serializer_class = InventorySerializer
+    queryset = Inventory.objects.all()
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
