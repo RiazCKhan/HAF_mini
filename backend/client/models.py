279b773e016d8e04527aa7982a15704e86dbfaba
RiazK <RiazCKhan@gmail.com>
2024-05-10 13:43:39 -0400

    add field property, unique

--- backend/client/models.py
+++ backend/client/models.py
@@ -3,7 +3,7 @@ from agent.models import Agent
 
 
 class Client(models.Model):
-    full_name = models.CharField(max_length=100)
+    full_name = models.CharField(unique=True, max_length=100)
     agent = models.ForeignKey(Agent, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=100)
     address = models.CharField(max_length=100)
