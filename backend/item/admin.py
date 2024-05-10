b32ba03e1a0ef1e37466248a5f99e00a53553ed1
RiazK <RiazCKhan@gmail.com>
2024-05-10 09:29:31 -0400

    add list display to admin panel

--- backend/item/admin.py
+++ backend/item/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Item, Inventory
 
-# Register your models here.
+class ItemAdmin(admin.ModelAdmin):
+  list_display = ("title", "price", "category")
+
+admin.site.register(Item, ItemAdmin)
+
+
+class InventoryAdmin(admin.ModelAdmin):
+  list_display = ("item", "stock",)
+
+admin.site.register(Inventory, InventoryAdmin)
