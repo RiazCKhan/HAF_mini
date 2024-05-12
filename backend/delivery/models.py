20e07ea045489c7783a19bbb1ee68df1736e170d
RiazK <RiazCKhan@gmail.com>
2024-05-12 17:23:54 -0400

    add delivery model to test referral

--- backend/delivery/models.py
+++ backend/delivery/models.py
@@ -1,8 +1,22 @@
 from django.db import models
+from referral.models import Referral
+
+
+STATUS_CHOICES = (
+    ("DRAFT", "draft"),
+    ("PENDING", "pending"),
+    ("APPROVED", "approved"),
+    ("REQUESTED", "requested"),
+    ("COMPLETE", "complete"),
+)
 
 
 class Delivery(models.Model):
-    pass
+    referral = models.ForeignKey(Referral, on_delete=models.CASCADE)
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, blank=True, null=True
+    )
+    date = models.DateField(blank=True, null=True)
 
     class Meta:
         verbose_name_plural = "Deliveries"
